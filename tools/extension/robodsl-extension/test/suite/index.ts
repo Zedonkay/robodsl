d1e66ff28f7ab9724df2fc5275da4cb69064de40
Zedonkay <ishikhar@andrew.cmu.edu>
2025-06-28 02:22:44 -0400

    many efforts on extension

--- tools/extension/robodsl-extension/test/suite/index.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-import { runTests } from 'vscode-test';
-
-export function run(): Promise<number> {
-    return runTests({
-        extensionDevelopmentPath: path.resolve(__dirname, '../../'),
-        extensionTestsPath: path.resolve(__dirname, './suite')
-    });
-} 
